8ef8e16d442a40a09ac48307ba5937f5c26cf2c9
LAPTOP-9DS8N2OJ\Asus <Treeruch@gmail.com>
2024-04-20 22:00:41 +0700

    push project

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const Account = require('./models/account');
+//เชื่่อมต่อmongodb//ชื่อnode-api-101
+mongoose.connect('mongodb://localhost:27017/node-api-101', {
+  useNewUrlParser: true
+});
+
+app.use(express.json());
+
+// mock data
+const account = [{}];
+
+app.post('/createAccount', async (req, res) => {
+  const payload = req.body;
+  const account = new Account(payload);
+  await account.save();
+
+  res.json({ status : 'Success' });
+  res.status(201).end();
+});
+
+app.get('/login', async (req, res) => {
+  const payload = req.body;
+  const accountObj = new Account(payload);
+
+  const account = await Account.findOne({ name: accountObj.name , password: accountObj.password});
+  res.json(account);
+});
+
+
+app.get('/account', async (req, res) => {
+  const account = await Account.find({});
+  res.json(account);
+});
+
+app.listen(9000, () => {
+  console.log('Application is running on port 9000');
+});
